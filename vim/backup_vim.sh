cc1bbd409fb9357d497e968aa527187d0fe446a6
Apo11oH <apo11odeving@gmail.com>
2014-11-16 18:55:59 -0800

    changed the vim backup script

--- vim/backup_vim.sh
+++ vim/backup_vim.sh
@@ -3,6 +3,7 @@ DIR="$HOME/.vim"
 if [ -d "$DIR" ]; then 
     rsync -u ~/.vimrc ./vimrc
     rsync -ur ~/.vim/* ./vim/
+    rm -rf ./vim/bundle
 else
     echo "VIM: could not find vim directory"
 fi
