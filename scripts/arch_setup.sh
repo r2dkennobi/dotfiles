9d1f7b3cdf66930f0f1ec19550ae9343f10cfc8d
Apo11oH <apo11odeving@gmail.com>
2015-03-23 12:14:47 -0700

    updated more stuff

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -30,10 +30,18 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # Install graphic stuff
 pacman -S xorg xterm xorg-server xorg-server-utils xorg-apps xf86-video-vesa xf86-video-intel xorg-xinit xorg-xclock
 # Install window manager
-pacman -S awesome
+pacman -S awesome i3
 # Install login manager
 pacman -S lightdm
 # Install tools
-pacman -S tmux git firefox gvim rxvt-unicode
+pacman -S tmux git firefox gvim rxvt-unicode wget volumeicon numlockx
 # Install network manager
-pacman -S networkmanager network-manager-applet xfce4-notifyd
+pacman -S networkmanager network-manager-applet xfce4-notifyd gnome-themes-standard
+
+# Add user
+useradd -m -G wheel -s /bin/bash apo11o
+sudo visudo
+
+# Install fonts
+pacman -S adobe-source-code-pro-fonts
+git clone https://github.com/powerline/fonts
