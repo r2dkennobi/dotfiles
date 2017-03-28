ca086e1da93f905c12ae51b23247ad2d4ca9f800
r2dkenny <r2dkenny@gmail.com>
2017-03-28 16:10:21 -0700

    Update bash config: Use gpg agent

--- bash/restore_bash.sh
+++ bash/restore_bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp ./bash_profile $HOME/.bash_profile
-cp ./xprofile $HOME/.xprofile
+#cp ./bash_profile $HOME/.bash_profile
+#cp ./xprofile $HOME/.xprofile
 echo "$HOME/dotfiles/bash/custom_config" >> ~/.bashrc
