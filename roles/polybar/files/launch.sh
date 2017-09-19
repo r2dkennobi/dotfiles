497a46d9996c9114c710bef43ed7719e5f8f4b05
R2DKennobi <r2dkennobi@tuta.io>
2017-09-19 07:17:11 -0700

    Update polybar configs

--- roles/polybar/files/launch.sh
+++ roles/polybar/files/launch.sh
@@ -3,6 +3,12 @@ killall -q polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar example &
+MONITOR=eDP-1 polybar example &
+if [ "$(xrandr | grep "DP-1 connected")" != "" ]; then
+  MONITOR=DP-1 polybar example &
+fi
+if [ "$(xrandr | grep "DP-2 connected")" != "" ]; then
+  MONITOR=DP-2 polybar example &
+fi
 #polybar top &
 #polybar bottom &
