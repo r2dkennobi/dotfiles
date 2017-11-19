01f06ffd77c7ea12c1a2b80e66c3dbafa943ed39
r2dkennobi <r2dkennobi@tuta.io>
2017-11-19 10:29:31 -0800

    Make Polybar multi-display launch smarter

--- roles/polybar/files/launch.sh
+++ roles/polybar/files/launch.sh
@@ -3,18 +3,10 @@ killall -q polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if [ "$(xrandr | grep "DP-1 connected")" != "" ]; then
-  MONITOR=DP-1 polybar example &
-fi
-if [ "$(xrandr | grep "DP-1-1 connected")" != "" ]; then
-  MONITOR=DP-1-1 polybar example &
-fi
-if [ "$(xrandr | grep "DP-1-2 connected")" != "" ]; then
-  MONITOR=DP-1-2 polybar example &
-fi
-if [ "$(xrandr | grep "DP-2 connected")" != "" ]; then
-  MONITOR=DP-2 polybar example &
-fi
-MONITOR=eDP-1 polybar example &
+MONITORS=$(xrandr | grep " connected" | awk '{ print $1 }' | sort)
+
+for m in $MONITORS; do
+  MONITOR=$m polybar example &
+done
 #polybar top &
 #polybar bottom &
