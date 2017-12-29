9a49d93dcc5d4985599de37a0713816b1d3595d8
r2dkennobi <r2dkennobi@tuta.io>
2017-12-29 10:09:07 -0800

    lxterminal: Remove configs

--- lxterminal/backup_lxterminal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$HOME/.config/lxterminal/"
-if [ -d $DIR ]; then
-    rsync -u $DIR/lxterminal.conf .
-else
-    echo "lxterminal configuration file not found"
-fi
--- lxterminal/restore_lxterminal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$HOME/.config/lxterminal"
-if [ ! -d $DIR ]; then
-    mkdir -p $DIR
-fi
-cp ./lxterminal.conf $DIR/lxterminal.conf
