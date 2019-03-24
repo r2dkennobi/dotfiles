2f20a3697651145d213efa02a48a288fe3d9197a
r2dkennobi <r2dkennobi@tuta.io>
2019-03-24 10:23:15 -0700

    Add caffeine to keep screen on

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,7 +21,7 @@ if [[ "$OS" == *"antergos"* ]]; then
   fi
 
   echo "- Install packages from AUR"
-  AUR_PKGS=(otf-ipaexfont google-chrome paman pavumeter keybase-bin)
+  AUR_PKGS=(otf-ipaexfont google-chrome paman pavumeter keybase-bin caffeine-ng)
   MISSING_PKGS=()
   for pkg in "${AUR_PKGS[@]}"; do
     if ! pacman -Qs "$pkg" > /dev/null; then
