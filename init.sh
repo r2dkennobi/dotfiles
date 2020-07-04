3ff611ef591d39ebb33df35619bfb333650e9a38
r2dkennobi <r2dkennobi@tuta.io>
2020-07-04 08:40:39 -0700

    Reduce deps for init setup

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CURDIR=$(pwd)
+while IFS= read -r -d '' file
+do
+  f=$(basename "$file")
+  ln -sfn "$file" "${HOME}/$f"
+done < <(find "$CURDIR" -name ".*" -not -name ".gitignore" -not -name ".git" -not -name ".gnupg" -print0)
+mkdir -p ~/.config/i3
+ln -sfn "${CURDIR}/i3/config" "${HOME}/.config/i3/config"
+ln -sfn "${CURDIR}/polybar" "${HOME}/.config/polybar"
+mkdir -p ~/.gnupg
+ln -sfn "${CURDIR}/.gnupg/gpg.conf" "${HOME}/.gnupg/gpg.conf"
+ln -sfn "${CURDIR}/.gnupg/gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf"
+ln -sfn "${CURDIR}/star_wars_ahsoka_by_wojtekfus.png" "${HOME}/lockscreen.png"
+ln -sfn "${CURDIR}/star_wars_peacekeeper_obi_wan_kenobi_by_thetechromancer.png" "${HOME}/background.png"
